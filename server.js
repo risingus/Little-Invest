b5b8539327ebe1efbfb8149ba701afe99543cf59
Gustavo Lima <gustavo.it@outlook.com>
2020-09-23 12:04:45 -0300

    1. Ajustando organização dos arquivos para subir para o gitpages

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
     server.use(express.static('public'))
         .get("/", (req,res) => {
-            return res.sendFile(__dirname + "/views/index.html")
+            return res.sendFile(__dirname + "/index.html")
         })
 
 .listen(5000)
